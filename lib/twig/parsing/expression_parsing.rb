78d43384d4c065362bfbd7deeffcc78f83655f53
James Tunnell <jamestunnell@gmail.com>
2015-10-30 15:32:00 -0700

    Add support for parsing function and method calls (and call chains) as expressions, with tests

--- lib/twig/parsing/expression_parsing.rb
+++ lib/twig/parsing/expression_parsing.rb
@@ -29,23 +29,35 @@ module Expression
     end
 
     i0 = index
-    r1 = _nt_function_call
+    r1 = _nt_method_call
     if r1
       r1 = SyntaxNode.new(input, (index-1)...index) if r1 == true
       r0 = r1
     else
-      r2 = _nt_identifier
+      r2 = _nt_call_chain
       if r2
         r2 = SyntaxNode.new(input, (index-1)...index) if r2 == true
         r0 = r2
       else
-        r3 = _nt_literal
+        r3 = _nt_grouped_expression
         if r3
           r3 = SyntaxNode.new(input, (index-1)...index) if r3 == true
           r0 = r3
         else
-          @index = i0
-          r0 = nil
+          r4 = _nt_identifier
+          if r4
+            r4 = SyntaxNode.new(input, (index-1)...index) if r4 == true
+            r0 = r4
+          else
+            r5 = _nt_literal
+            if r5
+              r5 = SyntaxNode.new(input, (index-1)...index) if r5 == true
+              r0 = r5
+            else
+              @index = i0
+              r0 = nil
+            end
+          end
         end
       end
     end
@@ -55,13 +67,84 @@ module Expression
     r0
   end
 
-  module FunctionCall0
-    def another
-      elements[3]
+  module MethodCall0
+    def reciever
+      elements[0]
+    end
+
+    def call_chain
+      elements[2]
     end
   end
 
-  module FunctionCall1
+  def _nt_method_call
+    start_index = index
+    if node_cache[:method_call].has_key?(index)
+      cached = node_cache[:method_call][index]
+      if cached
+        node_cache[:method_call][index] = cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
+    end
+
+    i0, s0 = index, []
+    i1 = index
+    r2 = _nt_grouped_expression
+    if r2
+      r2 = SyntaxNode.new(input, (index-1)...index) if r2 == true
+      r1 = r2
+    else
+      r3 = _nt_identifier
+      if r3
+        r3 = SyntaxNode.new(input, (index-1)...index) if r3 == true
+        r1 = r3
+      else
+        r4 = _nt_literal
+        if r4
+          r4 = SyntaxNode.new(input, (index-1)...index) if r4 == true
+          r1 = r4
+        else
+          @index = i1
+          r1 = nil
+        end
+      end
+    end
+    s0 << r1
+    if r1
+      if (match_len = has_terminal?(".", false, index))
+        r5 = true
+        @index += match_len
+      else
+        terminal_parse_failure('"."')
+        r5 = nil
+      end
+      s0 << r5
+      if r5
+        r6 = _nt_call_chain
+        s0 << r6
+      end
+    end
+    if s0.last
+      r0 = instantiate_node(SyntaxNode,input, i0...index, s0)
+      r0.extend(MethodCall0)
+    else
+      @index = i0
+      r0 = nil
+    end
+
+    node_cache[:method_call][start_index] = r0
+
+    r0
+  end
+
+  module CallChain0
+    def call
+      elements[1]
+    end
+  end
+
+  module CallChain1
     def first
       elements[0]
     end
@@ -71,7 +154,82 @@ module Expression
     end
   end
 
-  module FunctionCall2
+  def _nt_call_chain
+    start_index = index
+    if node_cache[:call_chain].has_key?(index)
+      cached = node_cache[:call_chain][index]
+      if cached
+        node_cache[:call_chain][index] = cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
+    end
+
+    i0, s0 = index, []
+    r1 = _nt_call
+    s0 << r1
+    if r1
+      s2, i2 = [], index
+      loop do
+        i3, s3 = index, []
+        if (match_len = has_terminal?(".", false, index))
+          r4 = true
+          @index += match_len
+        else
+          terminal_parse_failure('"."')
+          r4 = nil
+        end
+        s3 << r4
+        if r4
+          r5 = _nt_call
+          s3 << r5
+        end
+        if s3.last
+          r3 = instantiate_node(SyntaxNode,input, i3...index, s3)
+          r3.extend(CallChain0)
+        else
+          @index = i3
+          r3 = nil
+        end
+        if r3
+          s2 << r3
+        else
+          break
+        end
+      end
+      r2 = instantiate_node(SyntaxNode,input, i2...index, s2)
+      s0 << r2
+    end
+    if s0.last
+      r0 = instantiate_node(SyntaxNode,input, i0...index, s0)
+      r0.extend(CallChain1)
+    else
+      @index = i0
+      r0 = nil
+    end
+
+    node_cache[:call_chain][start_index] = r0
+
+    r0
+  end
+
+  module Call0
+    def another
+      elements[3]
+    end
+  end
+
+  module Call1
+    def first
+      elements[0]
+    end
+
+    def more
+      elements[1]
+    end
+  end
+
+  module Call2
     def identifier
       elements[0]
     end
@@ -82,12 +240,12 @@ module Expression
 
   end
 
-  def _nt_function_call
+  def _nt_call
     start_index = index
-    if node_cache[:function_call].has_key?(index)
-      cached = node_cache[:function_call][index]
+    if node_cache[:call].has_key?(index)
+      cached = node_cache[:call][index]
       if cached
-        node_cache[:function_call][index] = cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        node_cache[:call][index] = cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
         @index = cached.interval.end
       end
       return cached
@@ -165,7 +323,7 @@ module Expression
               end
               if s9.last
                 r9 = instantiate_node(SyntaxNode,input, i9...index, s9)
-                r9.extend(FunctionCall0)
+                r9.extend(Call0)
               else
                 @index = i9
                 r9 = nil
@@ -181,7 +339,7 @@ module Expression
           end
           if s6.last
             r6 = instantiate_node(SyntaxNode,input, i6...index, s6)
-            r6.extend(FunctionCall1)
+            r6.extend(Call1)
           else
             @index = i6
             r6 = nil
@@ -220,13 +378,13 @@ module Expression
     end
     if s0.last
       r0 = instantiate_node(SyntaxNode,input, i0...index, s0)
-      r0.extend(FunctionCall2)
+      r0.extend(Call2)
     else
       @index = i0
       r0 = nil
     end
 
-    node_cache[:function_call][start_index] = r0
+    node_cache[:call][start_index] = r0
 
     r0
   end
@@ -325,6 +483,86 @@ module Expression
     end
 
     node_cache[:arg][start_index] = r0
+
+    r0
+  end
+
+  module GroupedExpression0
+    def expression
+      elements[2]
+    end
+
+  end
+
+  def _nt_grouped_expression
+    start_index = index
+    if node_cache[:grouped_expression].has_key?(index)
+      cached = node_cache[:grouped_expression][index]
+      if cached
+        node_cache[:grouped_expression][index] = cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
+    end
+
+    i0, s0 = index, []
+    if (match_len = has_terminal?("(", false, index))
+      r1 = true
+      @index += match_len
+    else
+      terminal_parse_failure('"("')
+      r1 = nil
+    end
+    s0 << r1
+    if r1
+      s2, i2 = [], index
+      loop do
+        r3 = _nt_ws
+        if r3
+          s2 << r3
+        else
+          break
+        end
+      end
+      r2 = instantiate_node(SyntaxNode,input, i2...index, s2)
+      s0 << r2
+      if r2
+        r4 = _nt_expression
+        s0 << r4
+        if r4
+          s5, i5 = [], index
+          loop do
+            r6 = _nt_ws
+            if r6
+              s5 << r6
+            else
+              break
+            end
+          end
+          r5 = instantiate_node(SyntaxNode,input, i5...index, s5)
+          s0 << r5
+          if r5
+            if (match_len = has_terminal?(")", false, index))
+              r7 = true
+              @index += match_len
+            else
+              terminal_parse_failure('")"')
+              r7 = nil
+            end
+            s0 << r7
+          end
+        end
+      end
+    end
+    if s0.last
+      r0 = instantiate_node(SyntaxNode,input, i0...index, s0)
+      r0.extend(GroupedExpression0)
+    else
+      @index = i0
+      r0 = nil
+    end
+
+    node_cache[:grouped_expression][start_index] = r0
 
     r0
   end
--- spec/parsing/expression_parsing_spec.rb
+++ spec/parsing/expression_parsing_spec.rb
@@ -5,26 +5,55 @@ describe Parsing::ExpressionParser do
     @p = Parsing::ExpressionParser.new
   end
   
-  context 'given a valid literal string' do
-    it 'should parse' do
-      LITERAL_STRS.each {|s| expect(@p.parse(s)).to_not be_nil }
+  it 'should parse a valid literal string' do
+    LITERAL_STRS.each {|s| expect(@p.parse(s)).to_not be_nil }
+  end
+  
+  it 'should parse a valid identifier string' do
+    IDENTIFIER_STRS.each {|s| expect(@p.parse(s)).to_not be_nil }
+  end
+  
+  it 'should parse plain function calls' do
+    [
+      'myfunc()',
+      'do_xyz(1,2,3)',
+      'contact("bob",phone=1234567,email="bobbyboy@gogo.com")',
+    ].each do |str|
+      expect(@p.parse(str)).to_not be_nil
     end
   end
   
-  context 'given a valid identifier string' do
-    it 'should parse' do
-      IDENTIFIER_STRS.each {|s| expect(@p.parse(s)).to_not be_nil }
+  it 'should parse plain method calls' do
+    [
+      'abc.myfunc()',
+      'hey_you.do_xyz(1,2,3)',
+      'please.contact("bob",phone=1234567,email="bobbyboy@gogo.com")',
+    ].each do |str|
+      expect(@p.parse(str)).to_not be_nil
     end
   end
   
-  context 'given function calls' do
-    it 'shoould parse' do
-      [
-        'myfunc()',
-        'do_xyz(1,2,3)',
-        'contact("bob",phone=1234567,email="bobbyboy@gogo.com")',
+  it 'should parse a function call chain' do
+    [ 'please("john").do("my laundry").then("fold it")',
+      'run("over here").then_add(123, 456)'
+    ].each do |str|
+      expect(@p.parse(str)).to_not be_nil
+    end
+  end
+  
+  it 'should parse a method call chain' do
+    [ 'pretty.please("john").do("my laundry").then("fold it")',
+      'you.run("over here").then_add(123, 456)'
+    ].each do |str|
+      expect(@p.parse(str)).to_not be_nil
+    end
+  end
+  
+  context 'a grouped expression' do
+    it 'should parse' do
+      [ '(hey.you("over there"))','(123)','("hello")'
       ].each do |str|
-        
+        expect(@p.parse(str)).to_not be_nil
       end
     end
   end
