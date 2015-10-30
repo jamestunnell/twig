46754d0cb8461ef5d170130f443702c2304cff4d
James Tunnell <jamestunnell@gmail.com>
2015-10-30 13:03:48 -0700

    add helper objects

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,11 @@ end
 NUMERALS = [ "1","2","3","4","5","6","7","8","9" ]
 WIERD_CHARS = [ "*","(","&",",",".","<",">","?","#","@" ]
 
+IDENTIFIER_STRS = [
+  "WuLw65Y7PUJJl", "iBVHaB1DTI8Pt81p", "gKOfLmzXT", "OKKCPEK8ngnFLv6", "_TbD8",
+  "x5UtEu6", "aLYiqnQMJR", "PpxC4w", "L_pjD", "dZVRwfeZ", "B7como3UPfNL6", "fGa7u1hC_dG941"
+]
+
 BIN_DIGIT_STRS = [
   "0100011", "1011101100", "01000101011", "1011"
 ]
@@ -26,3 +31,18 @@ HEX_DIGIT_STRS = [
 DEC_DIGIT_STRS = [
   "123456789", "545512", "222135", "8390811", "021209593"
 ]
+
+RANDOM_INT_STRS = [
+  BIN_DIGIT_STRS.map {|s| "0b" + s },
+  HEX_DIGIT_STRS.map {|s| "0x" + s },
+  DEC_DIGIT_STRS
+].flatten
+
+RANDOM_ASCII_STRS = [
+  '"xOK`SvSYBUKRBrhzfT"', '"zUc~UPEMizt\\JV~smzB"', '"{PH`rI[P"', '"_H\\kC"',
+  '"szKADA]Feq]Hw"', '"]zlXYyXh}"', '"Au]\\lMJMOXz^}U{D}"',
+  '"zSVUf^fH}^fTAQr~tZTHFB"', '"jmGuph|QLeEqAZf[RvD_]]"', '"{TfJbj|_WXHQgsnndQq"'
+]
+
+LITERAL_STRS = RANDOM_INT_STRS + RANDOM_ASCII_STRS
+EXPRESSION_STRS = LITERAL_STRS + IDENTIFIER_STRS
